c42be6d61e49d51c7b0b962985bf119324884f2b
asantesylvesterkwame <intelligentson17@gmail.com>
2023-05-06 11:38:45 +0000

    created and added elements to chatBox component

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import sampleCreateChatIcon from "./chat (1).png"
+
+const ChatBox = () => {
+  return (
+    <div className=' h-30 hover:bg-slate-500 transition focus:bg-slate-300 rounded-lg cursor-pointer flex p-2.5 gap-3 '>
+        <div className="ChatImageDiv flex justify-center items-center">
+            <img src={sampleCreateChatIcon} className='w-10' alt="" />
+        </div>
+        <div className="ChatInfoDiv px-3">
+            <div className="ChatTitleDiv ">
+                <h3 className='font-bold'>Test Chat</h3>
+            </div>
+            <div className="lastRecentChat">
+                <p className='text-sm'>Hello</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatBox
